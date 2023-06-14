a12e04a30b3e3afe44d2482aecd3086b024ea69a
efgeorge <enache_george99@yahoo.com>
2023-06-15 00:30:01 +0300

    general structure

---
+++ src/components/Previewer.js
@@ -0,0 +1,14 @@
+import "../styles/Previewer.scss";
+
+const Previewer = () => {
+    return (
+        <div id="previeWrapper">
+            <div id="toolbar"></div>
+            <div id="preview">
+            
+            </div>
+        </div>
+    )
+}
+
+export default Previewer;
---
+++ src/components/Typefield.js
@@ -0,0 +1,12 @@
+import "../styles/Typefield.scss";
+
+const Typefield = () => {
+    return (
+        <div id="editorWraper">
+            <div id="toolbar"></div>
+            <textarea id="editor"></textarea>
+        </div>
+    )
+}
+
+export default Typefield;
