9737b643154bc6658d92e06ca547d5ec4c1a2e0c
efgeorge <fgeorge.enache@gmail.com>
2023-07-03 13:25:22 +0300

    merged marked converter with previewer

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,14 +1,19 @@
 import "../styles/Previewer.scss";
-import MarkedConverter from "../utils/MarkedConverter";
+import { useState } from "react";
+import { marked } from "marked";
 import Toolbar from "./Toolbar";
 
-const Previewer = ({ MarkedVal }) => {
+const Previewer = () => {
+    const [markedVal, setMarkedVal] = useState(
+        '# Default marked text\n' + "## This is a sub-heading...\n" + "Here is some code, `<div></div>`, between 2 backticks."
+    );
+    const renderer = new marked.Renderer();
+    const markdown = marked(markedVal, {sanitize: true});
+
     return (
         <div id="previeWrapper">
             <Toolbar title={"Previewer"} />
-            <div id="preview">
-                <MarkedConverter />
-            </div>
+            <div id="preview" dangerouslySetInnerHTML={{__html: marked(markdown, { renderer: renderer })}}></div>
         </div>
     )
 }
